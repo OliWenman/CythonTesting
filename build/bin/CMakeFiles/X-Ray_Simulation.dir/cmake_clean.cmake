b4ec12614872598ea8f08a58537fc2e23955aa6d
Wenman <xol73553@ws359.diamond.ac.uk>
2018-11-16 16:38:03 +0000

    Test

---
+++ build/bin/CMakeFiles/X-Ray_Simulation.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "run_static.c"
+  "sim_static.cxx"
+  "CMakeFiles/X-Ray_Simulation.dir/run_static.c.o"
+  "CMakeFiles/X-Ray_Simulation.dir/sim_static.cxx.o"
+  "CMakeFiles/X-Ray_Simulation.dir/__/src/Simulation.cc.o"
+  "CMakeFiles/X-Ray_Simulation.dir/__/src/Object.cc.o"
+  "X-Ray_Simulation.pdb"
+  "X-Ray_Simulation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/X-Ray_Simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xol73553/git/CythonTesting/src/Object.cc" "/home/xol73553/git/CythonTesting/build/src/CMakeFiles/sim.dir/Object.cc.o"
+  "/home/xol73553/git/CythonTesting/src/Simulation.cc" "/home/xol73553/git/CythonTesting/build/src/CMakeFiles/sim.dir/Simulation.cc.o"
+  "/home/xol73553/git/CythonTesting/build/src/sim.cxx" "/home/xol73553/git/CythonTesting/build/src/CMakeFiles/sim.dir/sim.cxx.o"
+  "/home/xol73553/git/CythonTesting/include/Simulation.hh" "/home/xol73553/git/CythonTesting/build/src/sim.cxx"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4UI_USE"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/scratch/sources/Anaconda2/include/python2.7"
+  "/dls_sw/apps/Geant4/geant4.10.04/geant4.10.04-install/include/Geant4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
